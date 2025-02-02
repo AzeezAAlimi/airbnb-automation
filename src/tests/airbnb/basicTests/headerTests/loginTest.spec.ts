b1ab9740f29fb9c4156ab02a238e00cec9a93d9a
AzeezAAlimi <azeezaalimi@gmail.com>
2025-02-02 17:08:16 +0100

    Fix

--- src/tests/airbnb/basicTests/headerTests/loginTest.spec.ts
+++ src/tests/airbnb/basicTests/headerTests/loginTest.spec.ts
@@ -13,25 +13,21 @@ test.beforeEach(async ({ page }) => {
 test.describe('Homepage login test', () => {
   test('Apple login test', async ({ page }) => {
     const homePage = new HomePage(page);
-    await loadHomePage(page);
     await homePage.header.navigationMenu.navMenuBtnLoggedout.loginSignup.appleLogin;
   });
 
   test('Facebook login test', async ({ page }) => {
     const homePage = new HomePage(page);
-    await loadHomePage(page);
     await homePage.header.navigationMenu.navMenuBtnLoggedout.loginSignup.facebookLogin();
   });
 
   test('Google login test', async ({ page }) => {
     const homePage = new HomePage(page);
-    await loadHomePage(page);
     await homePage.header.navigationMenu.navMenuBtnLoggedout.loginSignup.googleLogin();
   });
 
   test('Email login test', async ({ page }) => {
     const homePage = new HomePage(page);
-    await loadHomePage(page);
     await homePage.header.navigationMenu.navMenuBtnLoggedout.loginSignup.emailLogin(
       'qaairbnb0@gmail.com',
       'test12345?',
